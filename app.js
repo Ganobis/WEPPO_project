e11acbe27e3ceea57ef7906d45276887d95ef82a
lorakis <karol.cieslawski@gmail.com>
2021-01-31 20:11:55 +0100

    :(

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var express = require('express');
 var mysql = require('mysql');
+var cookieParser = require('cookie-parser');
+
 
 // Połączenie z bazą
 const db = mysql.createConnection({
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,30 @@ router.get('/', (req, res) => {
 
 });
 
+router.get( '/login', (req, res) => {
+    res.render('login');
+   });
+   
+router.post('/login', (req, res) => {
+    var username = req.body.login;
+    var pwd = req.body.password;
+
+    if (username == pwd) {
+        // wydanie ciastka
+        res.cookie('user', username, { signed: true });
+        // przekierowanie
+        var returnUrl = req.query.returnUrl;
+        res.redirect(returnUrl);
+    } else {
+        res.render('login', { message: "Zła nazwa logowania lub hasło" }
+        );
+    }
+});
+
+
+router.get('/register', (req, res) => {
+    res.render('/register')
+});
+
+
 module.exports = router;
